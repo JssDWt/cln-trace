d8ee53c9627be5408344d75f249a237260118a92
Jesse de Wit <witdejesse@hotmail.com>
2024-08-19 12:57:04 +0200

    include linux-gnu

--- build.rs
+++ build.rs
@@ -22,6 +22,8 @@ fn main() {
         .source(SRC)
         .clang_args([
             OsStr::new("-I"),
+            OsStr::new(&format!("/usr/include/aarch64-linux-gnu")),
+            OsStr::new("-I"),
             OsStr::new(&format!("/usr/src/linux-headers-6.1.0-23-common/arch/arm/include")),
             OsStr::new("-I"),
             OsStr::new(&format!("/usr/src/linux-headers-6.1.0-23-common/include")),
